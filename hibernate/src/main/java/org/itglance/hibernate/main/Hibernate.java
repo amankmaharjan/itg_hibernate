199171f73cb27d9fc221a106e13e331c0e6a3f07
nischalshakya15 <shakya_nischal15@hotmail.com>
2017-02-15 11:38:21 +0545

    Many to Many Unidirectional Mapping Finished

--- hibernate/src/main/java/org/itglance/hibernate/main/Hibernate.java
+++ hibernate/src/main/java/org/itglance/hibernate/main/Hibernate.java
@@ -5,56 +5,60 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.itglance.hibernate.entity.Department;
-import org.itglance.hibernate.entity.Employee;
+import org.itglance.hibernate.entity.Student;
+import org.itglance.hibernate.entity.Subject;
 
 public class Hibernate {
 
 	SessionFactory sf = new Configuration().configure().buildSessionFactory();
 
 	public void insert() {
-		Session session = sf.openSession();
-		session.beginTransaction();
-		Employee employee1 = new Employee();
-		Employee employee2 = new Employee();
+		try {
+			Session session = sf.openSession();
+			session.beginTransaction();
 
-		employee1.setFname("Hari");
-		employee1.setLname("Prasad");
+			Student student1 = new Student();
+			student1.setFname("Ram");
+			student1.setLname("Prasad");
 
-		employee2.setFname("Ram");
-		employee2.setLname("Sharma");
+			Subject subjects1 = new Subject();
+			subjects1.setSubjectName("Math");
 
-		Department department1 = new Department();
+			Subject subjects2 = new Subject();
+			subjects2.setSubjectName("Science");
 
-		department1.setDepartmentName("Human Resource");
-		employee1.setDepartment(department1);
-		employee2.setDepartment(department1);
-		department1.getEmployeeList().add(employee1);
-		department1.getEmployeeList().add(employee2);
+			student1.getSubjects().add(subjects1);
+			student1.getSubjects().add(subjects2);
 
-		session.save(department1);
+			session.save(student1);
 
-		session.getTransaction().commit();
-		session.close();
+			session.getTransaction().commit();
+			session.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 	public void update() {
 		Session session = sf.openSession();
 		session.beginTransaction();
-		Employee employeeUpdate = session.get(Employee.class, 1);
-		
-		
-		System.out.println("employee update info");
-		System.out.println(employeeUpdate.toString());
+		Student studentUpdate = session.get(Student.class, 1);
 
-		if (employeeUpdate != null) {
-			employeeUpdate.setFname("Ram");
-			employeeUpdate.setLname("Krisha");
-			employeeUpdate.getDepartment().setDepartmentName("Marketing");
-			session.update(employeeUpdate);
+		System.out.println("student info to be updated");
+		System.out.println(studentUpdate.toString());
+
+		if (studentUpdate != null) {
+			studentUpdate.setFname("Hari");
+			studentUpdate.setLname("Sharma");
+
+			for (Subject subjectsList : studentUpdate.getSubjects()) {
+				System.out.println("subject name " + subjectsList.getSubjectName());
+				// subjectsList.setSubjectName("new subject");
+				// studentUpdate.getSubjects().add(subjectsList);
+			}
 
 		}
-		
 		session.getTransaction().commit();
 		session.close();
 	}
@@ -62,60 +66,37 @@ public class Hibernate {
 	public void delete() {
 		Session session = sf.openSession();
 		session.beginTransaction();
-		Employee employeetDelete = session.get(Employee.class, 1);
-		System.out.println("employee info to be deleted");		
-		System.out.println(employeetDelete.toString());
-		if (employeetDelete != null) {
-			session.delete(employeetDelete);
+		Student studentDelete = session.get(Student.class, 1);
+		System.out.println("student info to be deleted");
+		System.out.println(studentDelete.toString());
+		if (studentDelete != null) {
+			session.delete(studentDelete);
 		}
-		
 		session.getTransaction().commit();
 		session.close();
 	}
 
-	@SuppressWarnings("unchecked")
-	public void displayfromDepartment() {
+	public void display() {
 		Session session = sf.openSession();
 		session.beginTransaction();
-		List<Department> listOfEmployee = session.createQuery("from Department").getResultList();
-		System.out.println("employee information from department entity");
-		for (Department getDepartment : listOfEmployee) {
-			System.out.println("department " + getDepartment.getDepartmentName());
-			for (Employee employeeDepartment : getDepartment.getEmployeeList()) {
-				System.out.println("first name " + employeeDepartment.getFname());
-				System.out.println("last name " + employeeDepartment.getLname());
-			}
-		}
+		@SuppressWarnings("unchecked")
+		List<Student> displayStudent = session.createQuery("from Student").getResultList();
+		System.out.println("student info");
+		System.out.println(displayStudent.toString());
 		session.close();
 	}
 
-	@SuppressWarnings("unchecked")
-	public void displayfromEmployee() {
-		Session session = sf.openSession();
-		session.beginTransaction();
-		List<Employee> listOfEmployee = session.createQuery("from Employee").getResultList();
-		System.out.println("employee information from employee entity");
-		System.out.println(listOfEmployee.toString());
-	}
-
 	public static void main(String args[]) {
-		
+
 		Hibernate hibernate = new Hibernate();
 
-		System.out.println("employee insert");
+		System.out.println("studnet insert");
 		hibernate.insert();
-		hibernate.displayfromEmployee();
-		hibernate.displayfromDepartment();
-
-		System.out.println("employee update");
-		hibernate.update();
-		hibernate.displayfromEmployee();
-		hibernate.displayfromDepartment();
-
-		System.out.println("employee delete");
+		hibernate.display();
+		//hibernate.update();
+		hibernate.display();
 		hibernate.delete();
-		hibernate.displayfromEmployee();
-		hibernate.displayfromDepartment();
+		hibernate.display();
 	}
 
 }
